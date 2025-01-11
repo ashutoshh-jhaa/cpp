4d3ff6b3c4c9fc3e3ad0975ab415e00121562a6f
ash <ashutoshh.dev@gmail.com>
2025-01-11 15:14:55 +0530

    demonstrated use of unary operator for operator overloading

--- 03.Classes and Objects/operator-as-friend.cpp
+++ 03.Classes and Objects/operator-as-friend.cpp
@@ -20,16 +20,26 @@ class Complex {
   }
   // Friend function declaration
   friend Complex operator+(Complex, Complex);
+  friend Complex operator-(Complex);
 };
-// Friend function definition
+// Friend function definition for binary
 Complex operator+(Complex c1, Complex c2) {
-  Complex c3;
-  c3.x = c1.x + c2.x;
-  c3.y = c1.y + c2.y;
-  return c3;
+  Complex temp;
+  temp.x = c1.x + c2.x;
+  temp.y = c1.y + c2.y;
+  return temp;
+}
+
+// Friend function definition for unary
+Complex operator-(Complex c) {
+  Complex temp;
+  temp.x = -c.x;
+  temp.y = -c.y;
+  return temp;
 }
 
 int main() {
+  // binary demonstration
   Complex c1(88, 22), c2(10, 10), c3, c4;
   c3 = c1 + c2;  //+ Operator was called and c1 and c2 was passed as arguments
   c4 = operator+(
@@ -37,4 +47,11 @@ int main() {
 
   c3.displayValues();
   c4.displayValues();
+
+  // unary demonstration
+  Complex c5, c6;
+  c5 = -c4;            // - operator was called and c4 was passed as an argument
+  c6 = operator-(c1);  // same as - being called passing of c1 as an arguments
+  c5.displayValues();
+  c6.displayValues();
 }
--- 03.Classes and Objects/operator-member-function.cpp
+++ 03.Classes and Objects/operator-member-function.cpp
@@ -16,21 +16,39 @@ class Complex {
     y = b;
   }
 
-  void display() { cout << x << endl << y << endl; }
+  void displayValues() { cout << x << endl << y << endl; }
 
+  // binary operator member function
   Complex operator+(Complex c) {
-    Complex c1;
-    c1.x = x + c.x;
-    c1.y = y + c.y;
-    return c1;
+    Complex temp;
+    temp.x = x + c.x;
+    temp.y = y + c.y;
+    return temp;
+  }
+
+  // unary operator member function
+  Complex operator-() {
+    Complex temp;
+    temp.x = -x;
+    temp.y = -y;
+    return temp;
   }
 };
 int main() {
+  // Binary operator demonstration
   Complex c1(9, 10), c2(2, 3), c3, c4;
 
-  c3 = c1.operator+(c2);  // c1 called it's + operator and passed c2 as argument
-  c4 = c3 + c2;  // same as c3 calling it's + operator and passing c2 as
-                 // argument
-  c3.display();
-  c4.display();
+  c3 = c1 + c2;  // c1 calls its + operator, passing c2 as argument
+  c4 = c3 + c2;  // c3 calls its + operator, passing c2 as argument
+  c3.displayValues();
+  c4.displayValues();
+
+  // Unary operator demonstration
+  Complex c5, c6;
+  c5 = -c4;             // c4 calls its - operator
+  c6 = c3.operator-();  // Explicit call to - operator
+  c5.displayValues();
+  c6.displayValues();
+
+  return 0;
 }
