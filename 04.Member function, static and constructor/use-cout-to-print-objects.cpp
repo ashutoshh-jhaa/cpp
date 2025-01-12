3e758bb8e81a739e0f8226029b921fc69a6ac6bb
ash <ashutoshh.dev@gmail.com>
2025-01-12 18:33:08 +0530

    more on chaining << operator

--- 04.Member function, static and constructor/use-cout-to-print-objects.cpp
+++ 04.Member function, static and constructor/use-cout-to-print-objects.cpp
@@ -19,14 +19,24 @@ class Person {
 // Overload << operator to print details of a Person object
 // Returning ostream& is necessary for chaining << operations
 ostream& operator<<(ostream& o, Person p) {
-  o << "age : " << p.age << "\nteeth : " << p.teeth;
+  o << "\nage : " << p.age << "\nteeth : " << p.teeth;
   return o;  // Return the ostream object to enable chaining
 }
 
 int main() {
-  Person p1(333, 23);
+  Person p1(333, 23), p2(23, 9);
 
-  cout << p1 << endl;  // Use overloaded << to print Person details
+  /*
+    If we do not return a reference to ostream from the overloaded << operator,
+    the next call in the chain would look like this: void << p2.
+    Since no function with a void parameter exists, the code will fail to
+    compile. By returning a reference to ostream, the chaining works as follows:
+    The first call (cout << p1) returns ostream&, which is then used in the
+    second call (<< p2). This allows ostream to be passed along with p2 for the
+    second operation, enabling chaining.
+  */
+  cout << p1 << p2
+       << endl;  // Use overloaded << to print details of both Person objects
 
   // Alternative usage (unconventional and not recommended):
   // p1 << cout;
