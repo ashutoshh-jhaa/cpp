564994752897d38f25cb0f8254cd0be091f75cd0
ash <ashutoshh.dev@gmail.com>
2025-02-05 15:53:00 +0530

    validate phone number

---
+++ 06.Exception handling/validate-phone-number.cpp
@@ -0,0 +1,36 @@
+// wap to validate number entered by the user and use try catch
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+bool validateNumber(const char* str);
+
+int main() {
+  char number[11];
+
+  cout << "Enter your Number: ";
+  cin.getline(number, 11);
+
+  try {
+    if (strlen(number) != 10) {
+      throw "Number must be exactly 10 digits.";
+    }
+    if (!validateNumber(number)) {
+      throw "Invalid input. Enter only numeric digits.";
+    }
+    cout << "Phone Number Validated!" << endl;
+  } catch (const char* e) {
+    cout << "Error: " << e << endl;
+  }
+
+  return 0;
+}
+
+bool validateNumber(const char* str) {
+  for (int i = 0; i < 10; i++) {
+    if (str[i] < '0' || str[i] > '9') {
+      return false;
+    }
+  }
+  return true;
+}
