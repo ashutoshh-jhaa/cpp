c38f99f37561ba39586bf2256584dc2cd478817c
ash <ashutoshh.dev@gmail.com>
2025-02-18 18:59:30 +0530

    multilevel inheritance in cpp

---
+++ 09.Inheritance/multilevel-inheritance.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class A {
+  // Base class
+ public:
+  A() { cout << "default constructor from A\n"; }
+};
+
+class B : public A {
+  // Derived from A
+ public:
+  B() { cout << "default constructor from B\n"; }
+};
+
+class C : public B {
+  // Derived from B
+ public:
+  C() { cout << "default constructor from C\n"; }
+};
+
+int main() {
+  C c;
+}
